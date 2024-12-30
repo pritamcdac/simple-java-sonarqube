9bc642e8028f04ea2078a0d71a89a9c6760befb5
pritamde@cybage <93193350+pritamcdac@users.noreply.github.com>
2024-12-30 12:00:48 +0530

    Create example.sh

---
+++ scripts/example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Example script for GitHub Actions
+
+echo "Hello from example.sh script!"
+echo "Today's date is: $(date)"
+echo "The current working directory is: $(pwd)"
+echo "List of files in the directory:"
+ls -l
